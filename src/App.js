27ffa9232605aa026cffb57c00d98c95fb8e7792
Anubhav Bhattacharya <anubhav.bhattacharya0026@gmail.com>
2023-06-26 19:34:51 +0530

    delete tasks func added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,30 @@ function App() {
     setTasks(prevState => [ ... prevState, task])
   }
 
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t => t.id !== id));
+  }
+
+  const toggleTask = (id) =>{
+    setTasks(prevState => prevState.map(t => (
+      t.id === id ? { ...t, checked: !t.checked}
+      : t
+      )))
+  }
+
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask = {addTask}/>
-      {tasks && <TaskList tasks={tasks}/>}
+      {tasks && (
+        <TaskList 
+          tasks={tasks}
+          deleteTask = {deleteTask}
+          toggleTask = {toggleTask}
+          />
+      )}
     </div>
   );
 }
